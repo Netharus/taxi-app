1ea02673f94abd997cd70940f2d0cbb60ae4eb0a
Netharus <nekromant12331@gmail.com>
2024-09-23 19:27:37 +0300

    Add validation Pattern to DriverCreateDto

--- driver/src/main/java/com/example/driver/dto/DriverCreateDto.java
+++ driver/src/main/java/com/example/driver/dto/DriverCreateDto.java
@@ -3,6 +3,7 @@ package com.example.driver.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 import java.util.List;
 
@@ -13,7 +14,10 @@ public record DriverCreateDto(
         String email,
         @NotNull @NotEmpty
         String fullName,
-        @NotNull @NotEmpty
+        @Pattern(
+                regexp = "^\\+375(29|33|25)\\d{7}$",
+                message = "Phone number must be in the format +375XX0000000, where XX is 29, 33, or 25"
+        )
         String phoneNumber,
         @NotNull @NotEmpty
         String gender,
