ca39d676c8c1654866666e393fda00c734462aa6
Netharus <nekromant12331@gmail.com>
2024-09-24 18:51:11 +0300

    Update Rating class

--- passenger/src/main/java/com/example/passenger/model/Rating.java
+++ passenger/src/main/java/com/example/passenger/model/Rating.java
@@ -1,7 +1,17 @@
 package com.example.passenger.model;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 
 @Entity
 @Getter
