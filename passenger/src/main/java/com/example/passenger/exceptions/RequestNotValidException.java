47206b0894531770aaa1d75c7d592a1cdc099659
Netharus <nekromant12331@gmail.com>
2024-09-24 23:07:55 +0300

    Formatted exception

--- passenger/src/main/java/com/example/passenger/exceptions/RequestNotValidException.java
+++ passenger/src/main/java/com/example/passenger/exceptions/RequestNotValidException.java
@@ -2,15 +2,11 @@ package com.example.passenger.exceptions;
 
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.Set;
 
 @Data
 @RequiredArgsConstructor
 public class RequestNotValidException extends RuntimeException {
-    private final Set<String> errorMassages;
-
-
+    private final Set<String> errorMessages;
 }
