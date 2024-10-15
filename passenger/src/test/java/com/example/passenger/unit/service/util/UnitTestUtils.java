690a1c6d6f97cded0e0f4c2ac32be523c2b80dcb
Netharus <nekromant12331@gmail.com>
2024-10-15 14:39:47 +0300

    Add unit test to passengerService

---
+++ passenger/src/test/java/com/example/passenger/unit/service/util/UnitTestUtils.java
@@ -0,0 +1,59 @@
+package com.example.passenger.unit.service.util;
+
+import com.example.passenger.dto.PassengerCreateDto;
+import com.example.passenger.dto.PassengerResponseDto;
+import com.example.passenger.dto.PassengerUpdateDto;
+import com.example.passenger.dto.RatingCreateDto;
+import com.example.passenger.model.Passenger;
+import com.example.passenger.model.Rating;
+
+public class UnitTestUtils {
+    public static PassengerCreateDto getPassengerCreateDto() {
+        return PassengerCreateDto.builder()
+                .email("email@email.com")
+                .firstName("firstName")
+                .phoneNumber("phoneNumber")
+                .build();
+    }
+
+    public static PassengerUpdateDto getPassengerUpdateDto() {
+        return PassengerUpdateDto.builder()
+                .email("email@email.com")
+                .firstName("firstName")
+                .phoneNumber("phoneNumber")
+                .build();
+    }
+
+    public static PassengerResponseDto getPassengerResponseDto() {
+        return PassengerResponseDto.builder()
+                .id(1L)
+                .firstName("firstName")
+                .email("email@email.com")
+                .phoneNumber("phoneNumber")
+                .grade(5.)
+                .build();
+    }
+    public static Passenger getPassenger(){
+        return Passenger.builder()
+                .id(1L)
+                .firstName("firstName")
+                .email("email@email.com")
+                .phoneNumber("phoneNumber")
+                .grade(5.)
+                .build();
+    }
+    public static RatingCreateDto getRatingCreateDto() {
+        return RatingCreateDto.builder()
+                .driverId(1L)
+                .grade(5)
+                .passengerId(1L)
+                .build();
+    }
+    public static Rating getRating() {
+        return Rating.builder()
+                .passenger(getPassenger())
+                .grade(5)
+                .driverId(1L)
+                .build();
+    }
+}
